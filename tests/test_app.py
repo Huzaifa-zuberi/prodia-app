4e85ca0da4af29acb0883a9938a4f7a812da7dda
Huzaifa Zuberi <mhuzaifazuberi@gmail.com>
2026-07-06 16:59:52 -0700

    Add unit tests

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from server import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index_page(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
